0dbde95ad65d55f19f6e3b57dc19a3c065c39040
MJ <mhmdjahmad99@gmail.com>
2022-05-09 01:11:24 +0300

    applied cleaning on the sample website

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,6 +1,16 @@
 import pandas as pd
 from fake_useragent import UserAgent
+from bs4 import BeautifulSoup as bf
+from googletrans import Translator
 import requests
+import re
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.feature_selection import chi2
+import numpy as np
 
 ## Downloading nltk stopwords on first run
 # nltk.download('stopwords')
@@ -28,5 +38,28 @@ url = 'http://' + df_websites['tld'][2305]
 print(url)
 raw_html = requests.get(url, headers=header, timeout=5, verify=False)
 
-with open('sample_content.txt', 'w') as f:
-    f.write(str(raw_html.content))
+
+soup = bf(str(raw_html.content),"html5lib")
+for tag in soup(["style","script"]):    
+    tag.decompose()
+text = soup.get_text(separator='\n')
+text = text[2:-2]
+for char in ['\\n', '\\t', '\\r','\\x']:
+    text = text.replace(char, '')
+text = re.compile(r"\s+").sub(" ",text).strip().lower()
+text = re.sub('[^a-z ]','',text)
+text = text.split()
+stopwords = stopwords.words('english')
+wordnet_lemmatizer = WordNetLemmatizer()
+words = [wordnet_lemmatizer.lemmatize(word) for word in text if word not in stopwords and re.match("[a-z][a-z][a-z][a-z]+", word)]
+words = list(dict.fromkeys(words))
+print(words)
+print(len(words))
+
+str = ""
+for word in words:
+    str += word + " "
+
+with open('sample_content.dat', 'w') as f:
+    f.write(str)
+
