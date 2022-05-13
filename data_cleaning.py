2ba53862da489f63602671df2c6df1b31a07ec40
MJ <mhmdjahmad99@gmail.com>
2022-05-13 21:08:37 +0300

    completed cleaning the data set

--- data_cleaning.py
+++ data_cleaning.py
@@ -12,7 +12,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.feature_selection import chi2
 import numpy as np
 
-## Downloading nltk stopwords on first run
+# Downloading nltk stopwords on first run
 # nltk.download('stopwords')
 # nltk.download('punkt')
 # nltk.download('wordnet')
@@ -30,36 +30,51 @@ def convert_dtype(x):
     except:        
         return ''
 
+
+
 df_websites = pd.read_csv("{}".format(csv_path), skipinitialspace=True, usecols=["tld","industry"], converters={'tld': convert_dtype,'industry': convert_dtype})
 
-ua = UserAgent()
-header = {'User-Agent':str(ua.chrome), "Upgrade-Insecure-Requests": "1","DNT": "1","Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8","Accept-Encoding": "gzip, deflate"}
-url = 'http://' + df_websites['tld'][2305]
-print(url)
-raw_html = requests.get(url, headers=header, timeout=5, verify=False)
 
 
-soup = bf(str(raw_html.content),"html5lib")
-for tag in soup(["style","script"]):    
-    tag.decompose()
-text = soup.get_text(separator='\n')
-text = text[2:-2]
-for char in ['\\n', '\\t', '\\r','\\x']:
-    text = text.replace(char, '')
-text = re.compile(r"\s+").sub(" ",text).strip().lower()
-text = re.sub('[^a-z ]','',text)
-text = text.split()
-stopwords = stopwords.words('english')
-wordnet_lemmatizer = WordNetLemmatizer()
-words = [wordnet_lemmatizer.lemmatize(word) for word in text if word not in stopwords and re.match("[a-z][a-z][a-z][a-z]+", word)]
-words = list(dict.fromkeys(words))
-print(words)
-print(len(words))
+categories = [category for category in df_websites['industry'] if '[' not in category and category != '']
+categories = list(dict.fromkeys(categories))
 
-str = ""
-for word in words:
-    str += word + " "
+df_websites= df_websites[df_websites.industry.isin(categories)]
 
-with open('sample_content.dat', 'w') as f:
-    f.write(str)
+web = []
+cat = []
+words=[]
+for tld, industry in df_websites.itertuples(index=False):
+    ua = UserAgent()
+    header = {'User-Agent':str(ua.chrome), "Upgrade-Insecure-Requests": "1","DNT": "1","Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8","Accept-Encoding": "gzip, deflate"}
+    try:
+        url = 'http://' + tld
+        print(url)
+        raw_html = requests.get(url, headers=header, timeout=5, verify=False)
+    except: 
+        continue
+    soup = bf(str(raw_html.content),"html5lib")
+    for tag in soup(["style","script"]):
+        tag.decompose()
+    text = soup.get_text(separator='\n')
+    text = text[2:-2]
+    for char in ['\\n', '\\t', '\\r','\\x']:
+        text = text.replace(char, '')
+    text = re.compile(r"\s+").sub(" ",text).strip().lower()
+    text = re.sub('[^a-z ]','',text)
+    text = text.split()
+    # print(text)
+    all_stopwords = stopwords.words('english')
+    all_stopwords.extend(["email","home","contact","website","online","news","phone","copyright","reserved","sitemap","newsletter","navigation","article",""])
+    wordnet_lemmatizer = WordNetLemmatizer()
+    text = [wordnet_lemmatizer.lemmatize(word) for word in text if word not in all_stopwords and re.match("^[a-z]{4,12}$", wordnet_lemmatizer.lemmatize(word))]
+    
+    web.append(tld)
+    words.append(text)    
+    cat.append(industry)
+    df_results = pd.DataFrame({'Category':list(cat), 'Website':list(web), 'Cleaned':list(words)},columns=['Category','Website', 'Cleaned'])
+    df_results.to_csv('results.csv')
+
+
+
 
