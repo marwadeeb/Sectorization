b0f3b0db30fc393a1a9cdf84f53a7d061fd0d997
MJ <mhmdjahmad99@gmail.com>
2022-05-14 14:38:18 +0300

    created bag of words

---
+++ feature_engineering.py
@@ -0,0 +1,48 @@
+import pandas as pd 
+from sklearn import model_selection
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+import time
+
+## Handling unexpected input
+def convert_dtype(x):
+    if not x:
+        return ''
+    try:
+        return str(x)
+    except:        
+        return ''
+
+# start time
+start = time.time()
+
+## load data
+csv_path = "cleaned.csv"
+df_cleaned = pd.read_csv("{}".format(csv_path), skipinitialspace=True, usecols=['Category','Website','Cleaned'], converters={'Category': convert_dtype,'Website': convert_dtype,'Cleaned': convert_dtype}, encoding='latin-1')
+categories = [category for category in df_cleaned['Category']]
+categories = list(dict.fromkeys(categories))
+
+## split data into train and test
+df_train, df_test = model_selection.train_test_split(df_cleaned,test_size=0.10)
+
+## create bag of words for each industry
+vectorizer = CountVectorizer()
+bow = []
+for category in categories:
+    df_category = df_train[df_train['Category'] == category]
+    content = ""
+    for feature in df_category['Cleaned']:
+        content += feature + " "
+    content=content.replace("['","'")
+    content=content.replace("']","'")
+    content=content.replace("[]","")
+    content=content.replace("''","','")
+    content=content.replace("\n",",")
+    bow.append(content)
+X = vectorizer.fit_transform(bow)
+
+
+
+
+end = time.time()
+print(end-start)
+
