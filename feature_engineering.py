95a349fc5944a5905f86cad0db306185e879de1a
MJ <mhmdjahmad99@gmail.com>
2022-05-16 14:03:10 +0300

    refactored feature engineering

--- feature_engineering.py
+++ feature_engineering.py
@@ -4,7 +4,10 @@ from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 import time
 import pandas as pd
 import time
-from sklearn import model_selection
+import nltk
+import numpy as np
+from sklearn import model_selection, feature_selection
+import itertools as IT
 
 ## Handling unexpected input
 def convert_dtype(x):
@@ -19,35 +22,41 @@ def convert_dtype(x):
 start = time.time()
 
 ## load data
-csv_path = "cleaned.csv"
+csv_path = "sample.csv"
 df_cleaned = pd.read_csv("{}".format(csv_path), skipinitialspace=True, usecols=['Category','Website','Cleaned'], converters={'Category': convert_dtype,'Website': convert_dtype,'Cleaned': convert_dtype}, encoding='latin-1')
 categories = [category for category in df_cleaned['Category']]
 categories = list(dict.fromkeys(categories))
 
 ## split data into train and test
-df_train, df_test = model_selection.train_test_split(df_cleaned,test_size=0.10)
+df_train, df_test = model_selection.train_test_split(df_cleaned,test_size=0.05)
 
 ## create bag of words for each industry
-vectorizer = CountVectorizer()
 bow = []
 for category in categories:
     df_category = df_train[df_train['Category'] == category]
     content = ""
     for feature in df_category['Cleaned']:
         content += feature + " "
-    content=content.replace("['","'")
-    content=content.replace("']","'")
-    content=content.replace("[]","")
-    content=content.replace("''","','")
-    content=content.replace("\n",",")
     bow.append(content)
-X = vectorizer.fit_transform(bow)
 
-## apply tfidf vectorizer 
-X = vectorizer.fit_transform(bow)
-tfidf_vectorizer = TfidfVectorizer(max_df=0.8, min_df=0.2)
+# apply tfidf vectorizer  
+X = CountVectorizer().fit_transform(bow)
+tfidf_vectorizer = TfidfVectorizer(max_df=0.8, min_df=0.05)
 X_train = tfidf_vectorizer.fit_transform(bow) 
 X_names = tfidf_vectorizer.get_feature_names_out()
+print(X_names)
+
+## updated bow
+freq_train = X_train.toarray()
+u_bow = [] 
+for cat in freq_train:
+    features = []
+    for X_name, word_freq in zip(X_names, cat):
+        if word_freq != 0:
+            features.append(X_name)
+    u_bow.append(features)
+
+
 
 end = time.time()
 print(end-start)
